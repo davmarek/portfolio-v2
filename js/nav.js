8bb0bca93d55d364fc36cd4cba9465d9271deedf
davmarek <34945560+davmarek@users.noreply.github.com>
2021-03-27 14:46:00 +0100

    Fixed mobile nav + menu opening

--- js/nav.js
+++ js/nav.js
@@ -1,21 +1,41 @@
 //proměné menu control
 let menuOpen = document.getElementById("menuOpen");
 let menuClose = document.getElementById("menuClose");
+let menu = document.getElementById("menu");
+
+// menu offset adjust
+menu.style.right = "-100%";
+let adjustMenuOffset = () => {
+	let spaceFromRight = menu.style.right;
+	if (spaceFromRight == "-50%" || spaceFromRight == "-100%")
+		if (window.innerWidth > 767) menu.style.right = "-100%";
+		else menu.style.right = "-50%";
+};
+adjustMenuOffset();
+window.addEventListener("resize", adjustMenuOffset);
 
 //otevření menu
 menuOpen.addEventListener("click", () => {
 	gsap.to("#menu", {
 		duration: 0.5,
-		right: 0,
+		right: "0%",
 		ease: "power2.inOut",
 	});
 });
 
 //zavření menu
 menuClose.addEventListener("click", () => {
-	gsap.to("#menu", {
-		duration: 0.5,
-		right: "-100%",
-		ease: "power3.inOut",
-	});
+	if (window.innerWidth > 768)
+		gsap.to("#menu", {
+			duration: 0.5,
+			right: "-100%",
+			ease: "power3.inOut",
+		});
+	else {
+		gsap.to("#menu", {
+			duration: 0.5,
+			right: "-50%",
+			ease: "power3.inOut",
+		});
+	}
 });
